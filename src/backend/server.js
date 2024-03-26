b098829b6b03e01fd4d136d1a0a74b0b22a2867e
Adam Dezsi <info@adamdezsi.com>
2024-03-26 15:25:32 -0700

    backend logic mostly done, double voting filter implemented, though it's just an easy ip based one.

---
+++ src/backend/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 3030;
+const usedIPList = [];
+
+// our initial counter
+let count = 0;
+
+//doube voting filter middleware
+const doubleVotingEasyFilter = (req, res, next) => {
+  const IP = req.IP;
+  if (usedIPList.includes(IP)) {
+    return res.status(403).json({ error: "You have already voted." });
+  } else {
+    usedIPList.push(IP);
+    next();
+  }
+};
+
+// json parsing middleware
+app.use(bodyParser.json());
+
+app.get("/api/vote-count", (req, res) => {
+  res.json({ count: count });
+});
+
+app.post("/api/upvote", doubleVotingEasyFilter, (req, res) => {
+  count++;
+  res.json({ success: true });
+});
+
+app.post("/api/downvote", doubleVotingEasyFilter, (req, res) => {
+  count--;
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
